47d2c96426f47332239fd430245247edacfd08a4
Brian kipkoech <kipkoechbrian0@gmail.com>
2023-03-04 17:00:12 +0300

    menu component and links

--- src/components/MuiNav.tsx
+++ src/components/MuiNav.tsx
@@ -1,9 +1,24 @@
-import { CatchingPokemon } from "@mui/icons-material";
-import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import {
+  ArrowDownward,
+  CatchingPokemon,
+  ExpandMore,
+} from "@mui/icons-material";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Link,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Stack } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
 
 export default function MuiNav() {
+  const [anchor, setAnchor] = useState<null | HTMLElement>(null);
+  const [open, setOpen] = useState(false);
   return (
     <AppBar position="static">
       <Toolbar>
@@ -13,12 +28,66 @@ export default function MuiNav() {
         <Typography variant="h5" sx={{ flexGrow: 1 }}>
           DeckSTream
         </Typography>
-        <Stack direction={"row"} spacing={2}>
-          <Button color="inherit">Featured</Button>
-          <Button color="inherit">Pricing</Button>
-          <Button color="inherit">About</Button>
-          <Button color="inherit">Register</Button>
+        <Stack
+          textAlign={"center"}
+          alignItems="center"
+          direction={"row"}
+          spacing={2}
+        >
+          <Link underline="none" variant="button" href="#" color="inherit">
+            Pricing
+          </Link>
+          <Link underline="none" variant="button" href="#" color="inherit">
+            About
+          </Link>
+          <Link underline="none" variant="button" href="#" color="inherit">
+            Register
+          </Link>
+          <Button
+            color="inherit"
+            onClick={(e) => {
+              setAnchor(e.currentTarget);
+              setOpen(true);
+            }}
+            endIcon={<ExpandMore />}
+          >
+            Featured
+          </Button>
         </Stack>
+        <Menu
+          open={open}
+          id="menu"
+          anchorEl={anchor}
+          onClick={(e) => {
+            setOpen(false);
+            setAnchor(e.currentTarget);
+          }}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem
+            onClick={(e) => {
+              setOpen(false);
+              setAnchor(e.currentTarget);
+            }}
+          >
+            Blog
+          </MenuItem>
+          <MenuItem
+            onClick={(e) => {
+              setAnchor(e.currentTarget);
+              setOpen(false);
+            }}
+          >
+            Podcast
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
