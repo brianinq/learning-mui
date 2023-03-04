5b94f225080abfe805d739891cd3071c7de98ae1
Brian kipkoech <kipkoechbrian0@gmail.com>
2023-03-04 13:35:03 +0300

    paper component

--- src/components/MuiGrids.tsx
+++ src/components/MuiGrids.tsx
@@ -1,30 +1,32 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Paper } from "@mui/material";
 import React from "react";
 
 function MuiGrids() {
   return (
-    <Grid container spacing={1}>
-      <Grid item xs={6} sm={4}>
-        <Box bgcolor="blue" p={3} color="white">
-          Item 1
-        </Box>
+    <Paper elevation={10} sx={{ padding: "20px" }}>
+      <Grid container spacing={1}>
+        <Grid item xs={6} sm={4}>
+          <Box bgcolor="blue" p={3} color="white">
+            Item 1
+          </Box>
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <Box bgcolor="blue" p={3} color="white">
+            Item 2
+          </Box>
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <Box bgcolor="blue" p={3} color="white">
+            Item 3
+          </Box>
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <Box bgcolor="blue" p={3} color="white">
+            Item 4
+          </Box>
+        </Grid>
       </Grid>
-      <Grid item xs={6} sm={4}>
-        <Box bgcolor="blue" p={3} color="white">
-          Item 2
-        </Box>
-      </Grid>
-      <Grid item xs={6} sm={4}>
-        <Box bgcolor="blue" p={3} color="white">
-          Item 3
-        </Box>
-      </Grid>
-      <Grid item xs={6} sm={4}>
-        <Box bgcolor="blue" p={3} color="white">
-          Item 4
-        </Box>
-      </Grid>
-    </Grid>
+    </Paper>
   );
 }
 
